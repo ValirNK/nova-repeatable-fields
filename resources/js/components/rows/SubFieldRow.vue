f4671a920c5c43b0278e60a99b5aede1516f3966
ValirNK <top.trend2017@yandex.ru>
2022-12-27 17:37:44 +0300

    add ImageSubField

--- resources/js/components/rows/SubFieldRow.vue
+++ resources/js/components/rows/SubFieldRow.vue
@@ -38,6 +38,7 @@
     import NumberSubField from '../sub-fields/NumberSubField.vue';
     import SelectSubField from '../sub-fields/SelectSubField.vue';
     import TextareaSubField from '../sub-fields/TextareaSubField.vue';
+    import ImageSubField from '../sub-fields/ImageSubField.vue';
 
     export default {
 
@@ -47,6 +48,7 @@
             NumberSubField,
             SelectSubField,
             TextareaSubField,
+            ImageSubField
         },
 
         props: ['value', 'field', 'index'],
---
+++ resources/js/components/sub-fields/ImageSubField.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input
+            :id="subField.name"
+            :name="subField.name"
+            type="file"
+            class="w-full form-control form-input form-input-bordered"
+            v-bind="subField.attributes"
+        />
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+
+        props: [
+            'subField',
+            'value'
+        ],
+
+    }
+</script>
