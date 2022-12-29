d03a10643a5455ab0e7d7772bb32a6a878e09a80
ValirNK <top.trend2017@yandex.ru>
2022-12-29 14:35:47 +0300

    second commit2

--- resources/js/components/sub-fields/ImageSubField.vue
+++ resources/js/components/sub-fields/ImageSubField.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <file-upload :url="url" :id="id" :label="label"></file-upload>
+        <div
+          class="imagePreviewWrapper"
+          :style="{ 'background-image': `url(${previewImage})` }"
+          @click="selectImage">
+        </div>
+        <input
+          ref="fileInput"
+          type="file"
+          @input="pickFile"
+        >
         <!-- <input
             :id="subField.name"
             :name="subField.name"
@@ -9,116 +18,43 @@
             v-bind="subField.attributes"
         /> -->
     </div>
-
 </template>
 
 <script>
-  import {fileUpload} from '@yazan.alnughnugh/file-upload'
-  export default {
-    components: {
-        fileUpload
-    },
-    name: 'ImageUpload',
-    // props: [
-    //     'subField',
-    //     'value'
-    // ],
-    data() {
+export default {
+  data() {
       return {
-        // uploadedFiles: [],
-        // uploadError: null,
-        // currentStatus: null,
-        // uploadFieldName: 'photos'
+        previewImage: null
+      };
+    },
+  methods: {
+      selectImage () {
+          this.$refs.fileInput.click()
+      },
+      pickFile () {
+        let input = this.$refs.fileInput
+        let file = input.files
+        if (file && file[0]) {
+          let reader = new FileReader
+          reader.onload = e => {
+            this.previewImage = e.target.result
+          }
+          reader.readAsDataURL(file[0])
+          this.$emit('input', file[0])
+        }
       }
-    },
-    computed: {
-    //   isInitial() {
-    //     return this.currentStatus === STATUS_INITIAL;
-    //   },
-    //   isSaving() {
-    //     return this.currentStatus === STATUS_SAVING;
-    //   },
-    //   isSuccess() {
-    //     return this.currentStatus === STATUS_SUCCESS;
-    //   },
-    //   isFailed() {
-    //     return this.currentStatus === STATUS_FAILED;
-    //   }
-    },
-    methods: {
-    //   reset() {
-    //     // reset form to initial state
-    //     this.currentStatus = STATUS_INITIAL;
-    //     this.uploadedFiles = [];
-    //     this.uploadError = null;
-    //   },
-    //   save(formData) {
-    //     // upload data to the server
-    //     this.currentStatus = STATUS_SAVING;
-
-    //     upload(formData)
-    //       .then(x => {
-    //         this.uploadedFiles = [].concat(x);
-    //         this.currentStatus = STATUS_SUCCESS;
-    //       })
-    //       .catch(err => {
-    //         this.uploadError = err.response;
-    //         this.currentStatus = STATUS_FAILED;
-    //       });
-    //   },
-    //   filesChange(fieldName, fileList) {
-    //     // handle file changes
-    //     const formData = new FormData();
-
-    //     if (!fileList.length) return;
-
-    //     // append the files to FormData
-    //     Array
-    //       .from(Array(fileList.length).keys())
-    //       .map(x => {
-    //         formData.append(fieldName, fileList[x], fileList[x].name);
-    //       });
-
-    //     // save it
-    //     console.log(formData)
-    //     this.save(formData);
-    //   }
-    },
-    mounted() {
-    //   this.reset();
-    },
   }
-
+}
 </script>
 
-
-<style lang="scss">
-  .dropbox {
-    outline: 2px dashed grey; /* the dash box */
-    outline-offset: -10px;
-    background: lightcyan;
-    color: dimgray;
-    padding: 10px 10px;
-    // min-height: 200px; /* minimum height */
-    position: relative;
+<style scoped lang="scss">
+.imagePreviewWrapper {
+    width: 250px;
+    height: 250px;
+    display: block;
     cursor: pointer;
-  }
-  
-  .input-file {
-    opacity: 0; /* invisible but it's there! */
-    width: 100%;
-    // height: 200px;
-    position: absolute;
-    cursor: pointer;
-  }
-  
-  .dropbox:hover {
-    background: lightblue; /* when mouse over to the drop zone, change color */
-  }
-  
-  .dropbox p {
-    font-size: 1.2em;
-    text-align: center;
-    padding: 10px 0;
-  }
+    margin: 0 auto 30px;
+    background-size: cover;
+    background-position: center center;
+}
 </style>
