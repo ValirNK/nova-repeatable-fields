2a27009b346aa38e0608f910395167ff372b0f18
ValirNK <top.trend2017@yandex.ru>
2022-12-29 22:26:58 +0300

    second commit2

--- resources/js/components/sub-fields/ImageSubField.vue
+++ resources/js/components/sub-fields/ImageSubField.vue
@@ -54,9 +54,9 @@ export default {
             this.previewImage = e.target.result
           }
           reader.readAsDataURL(file[0])
-          this.imageValue = file[0].
+          this.imageValue = file[0]
           console.log(this.imageValue)
-          // this.$refs.uploadAvatar.input(this.previewImage)
+          this.$refs.uploadAvatar.input(this.previewImage)
           this.$emit('input', file[0])
         }
       }
